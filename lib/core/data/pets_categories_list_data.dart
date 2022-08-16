8a8fb9bbf75521b733b68cf1899e60357152d5b8
rayaniselim <rayaniselim@outlook.com>
2022-08-16 15:03:14 -0400

    edit commit  2

--- lib/core/data/pets_categories_list_data.dart
+++ lib/core/data/pets_categories_list_data.dart
@@ -2,19 +2,19 @@ import 'package:app_pet_fteam/core/core.dart';
 
 const List<PetCategoryModel> petsCategoriesListData = [
   PetCategoryModel(
-    race: 'Dogs',
+    species: 'Dogs',
     icon: 'assets/images/icons/png/dog.png',
   ),
   PetCategoryModel(
-    race: 'Cats',
+    species: 'Cats',
     icon: 'assets/images/icons/png/cat.png',
   ),
   PetCategoryModel(
-    race: 'Birds',
+    species: 'Birds',
     icon: 'assets/images/icons/png/bird.png',
   ),
   PetCategoryModel(
-    race: 'Others',
+    species: 'Others',
     icon: 'assets/images/icons/png/rabbit.png',
   ),
 ];
