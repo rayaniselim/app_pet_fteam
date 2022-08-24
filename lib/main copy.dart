d3e7d0617cea65b1355ab1a96dcd039ac03b1477
rayaniselim <rayaniselim@outlook.com>
2022-08-24 10:25:05 -0400

    4

--- lib/main copy.dart
+++ lib/main copy.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
-import 'app/app.dart';
+// import 'package:flutter/material.dart';
+// import 'app/app.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-        splashColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-      ),
-      home: const PetsListScreen(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.deepOrange,
+//         splashColor: Colors.transparent,
+//         highlightColor: Colors.transparent,
+//       ),
+//       home: const PetsListScreen(),
+//     );
+//   }
+// }
